ab8c4c8ad349ef1aa9aea74e925d1a8f32f9b5bd
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-10-17 01:37:24 +0900

    本の貸出画面の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_101842) do
+ActiveRecord::Schema.define(version: 2020_10_16_163213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 2020_10_16_101842) do
     t.date "lending_start_date", null: false
     t.date "lending_end_date", null: false
     t.integer "user_id", null: false
-    t.integer "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "book_id"
+    t.index ["book_id"], name: "index_rentals_on_book_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_10_16_101842) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rentals", "books"
 end
